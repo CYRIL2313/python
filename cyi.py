073b392a137e07be5c0f393dbc81f0540abe7966
CYRIL2313 <cyrilaccq@gmail.com>
2024-10-04 16:06:02 +0530

    Add files via upload
    Python Basics

---
+++ cyi.py
@@ -0,0 +1,83 @@
+''print("hello world")
+print(''HELLO')
+print("world")
+print("hello\nworld")
+if 5>2:
+    print("five greater than two")'''
+'''if 5>2:
+    print("five greater than two")
+if 5>2:
+     print("five greater than two")
+if 5 > 2:
+      print("five greater than two")'''
+
+'''if 5>2:
+    print("five greater than two")
+    print("five greater than two")'''
+
+'''x=4
+x="thomas"                #here thomas will get printed as thomas will over 
+print(x)'''               #right the firts value given to xprint(x)
+
+'''x="thomas"
+x=5
+print(x)'''
+
+'''x=str('5')# x will be '3'
+y=int(6)# y will be 6
+z=float(3)# z will be 3.0
+print(x,y,z)'''
+
+'''x=7.9
+y="python"
+print(type(x))
+print(type(y))'''
+
+'''x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)'''
+
+'''x=y=z="orange"
+print(x)
+print(y)
+print(z)'''
+
+
+'''x="python "
+y="is "
+z="awesome"
+print(x+y+z)'''
+
+'''username = input("Enter your name;")
+print("username is ; " + username)'''
+
+'''a="hello"
+b='world'
+c=a + b
+print(c)'''
+
+'''a="Hello, World"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
+print(a[7])
+print(a[8])
+print(a[9])
+print(a[10])
+print(a[11])'''
+
+b="Hello ,World"
+print(b[2:5])
+print(b[:5])
+print(b[1:])
+print(b[:13])
+
+
+
+
+
