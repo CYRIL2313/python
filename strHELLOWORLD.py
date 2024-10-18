06a970358d1eb24633b6c4339017ecb83f1fb4ed
CYRIL2313 <cyrilaccq@gmail.com>
2024-10-18 15:07:09 +0530

    Add files via upload
    A Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.

---
+++ strHELLOWORLD.py
@@ -0,0 +1,11 @@
+# Author:cyril
+# date:18/10/2024
+# Description:a Python program that stores a string in a variable.
+# Extract a specific part of the string (substring) and then concatenate it with another string.
+# Finally, display the new string.
+
+str_var = "Hello, world!"
+str_another_var = str_var[7:12]
+print(str_another_var)
+str_3rd_var=" Everyone!"
+print(str_another_var+str_3rd_var)
