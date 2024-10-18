93164ef61102def927eb80d4aa2f38df712339a9
CYRIL2313 <cyrilaccq@gmail.com>
2024-10-18 14:31:52 +0530

    Add files via upload
    A Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.

---
+++ BASICPYTHON.py
@@ -0,0 +1,9 @@
+#Author:cyril
+#date:18/10/2024
+#Description:program that asks the user to input their name and age. After receiving the input
+#,display a message that includes the user's name and age.
+#Output:Hello, John! You are 25 years old.
+
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("HELLO,",name+"! you are",age,"years old")
