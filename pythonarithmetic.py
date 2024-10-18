b3e40533913c3dbf11af19e3c3daaf845b0b1b00
CYRIL2313 <cyrilaccq@gmail.com>
2024-10-18 16:00:37 +0530

    Add files via upload
    A Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.

---
+++ pythonarithmetic.py
@@ -0,0 +1,11 @@
+# Author:cyril
+# date:18/10/2024
+# Description:A Python program that demonstrates the usage of arithmetic, comparison, and logical operators.
+# Perform a few operations and print the results.
+number1 = int(input("Enter your first number:"))
+number2 = int(input("Enter your second number:"))
+print("sum=",number1+number2,"Dividion=",number1/number2,)
+print("is number1 greater than number2?:",number1>number2)
+print("is number1=number2?",number1==number2)
+print("Logical AND",number1>0 and number2>0)
+print("Logical OR",number1>0 or number2>number1)
