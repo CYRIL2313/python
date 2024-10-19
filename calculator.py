31548d2e36bbfcf85898b7f18b735b1e91d89005
CYRIL2313 <cyrilaccq@gmail.com>
2024-10-19 14:49:18 +0530

    Add files via upload
    Simple desktop calculator using Python. Only the five basic arithmetic operators.

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#Author:cyril
+#Date:19-10-2024
+#Description:Simple desktop calculator using Python.
+#Only the five basic arithmetic operators.
+
+
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the thirt number:"))
+sum=(num1+num2)
+print("The sum of num1 and num2 is:result",sum)
+subtraction=(num1-num2)
+print("The subtraction of num1 and num2 is:result",subtraction)
+multiplication=(num1*num2)
+print("The multiplication  of num1 and num2 is:result",multiplication)
+division=(num1/num2)
+print("The quotient of num1 and num2 is:result",division)
+modules=(num1%num2)
+print("The reminder of num1 and num2 is:result",modules)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2:",result)
+
+
+
