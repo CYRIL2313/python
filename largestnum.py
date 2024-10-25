82d56b87ab86306289c4f38c62323fcdd8c400f4
CYRIL2313 <cyrilaccq@gmail.com>
2024-10-25 14:21:12 +0530

    Add files via upload
     Write a Python program to find the largest of two numbers. The program should take two numbers as input from the user and determine which one is the largest. Use conditional statements to compare the numbers.

---
+++ largestnum.py
@@ -0,0 +1,14 @@
+#Author:cyril
+#Date:25-10-2024
+#Description: Write a Python program to find the largest of two numbers.
+# The program should take two numbers as input from the user and determine which one is the largest.
+# Use conditional statements to compare the numbers.
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+if(x>y):
+    print(x,"is greater than",y)
+else :
+    print(y,"is greater than",x)
+
+
+
